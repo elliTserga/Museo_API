ad4bf65c3451144adb4fb24bef783428939aff76
elliTserga <ellitser@ee.duth.gr>
2026-07-09 12:42:17 +0300

    Refactor solution structure

--- Adapter/obj/Debug/net10.0/Adapter.AssemblyInfo.cs
+++ Adapter/obj/Debug/net10.0/Adapter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adapter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5305d32908b4c49a8342c48d302b3a13c52b6c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adapter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adapter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MuseoData/obj/Debug/net10.0/MuseoData.AssemblyInfo.cs
+++ MuseoData/obj/Debug/net10.0/MuseoData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuseoData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5305d32908b4c49a8342c48d302b3a13c52b6c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuseoData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuseoData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
